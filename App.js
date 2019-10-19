5357d17fc1685f21f62140388b71301352558c53
japrudtst@gmail.com <japrudtst@gmail.com>
2019-10-19 16:03:18 +0800

    firt setup

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import ArtistScreen from 'src/screens/ArtistScreen';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return <ArtistScreen />;
+  }
+}
